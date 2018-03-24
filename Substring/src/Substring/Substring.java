b2577452e02b7fa0e0ec8c348f7d023aa35c72b3
Bigoz005 <32684265+Bigoz005@users.noreply.github.com>
2018-03-24 18:35:27 +0100

    Add files via upload

---
+++ Substring/src/Substring/Substring.java
@@ -0,0 +1,20 @@
+package Substring;
+
+public class Substring {
+
+	public static void main(String[] args) {
+		if (args.length < 3) {
+			System.out.println("Za ma³o argumentów.");
+			return;
+		}
+		try {
+			int i = Integer.parseInt(args[1]);
+			int j = Integer.parseInt(args[2]);
+			System.out.println(args[0].substring(i, j + 1));
+		} catch (NumberFormatException error) {
+			System.out.println("Z³y format liczby: " + error.getLocalizedMessage());
+		} catch (StringIndexOutOfBoundsException error) {
+			System.out.println("Nieprawid³owe indeksy: " + error.getLocalizedMessage());
+		}
+	}
+}
